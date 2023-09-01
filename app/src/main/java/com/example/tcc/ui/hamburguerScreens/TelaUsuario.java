f50a25d64e8a228b6ba8615297cfcc4f2a2f0a0d
Rafael Cassaro <rafaelcassacosta@hotmail.com>
2023-08-31 22:15:57 -0300

    initial commit

---
+++ app/src/main/java/com/example/tcc/ui/hamburguerScreens/TelaUsuario.java
@@ -0,0 +1,16 @@
+package com.example.tcc.ui.hamburguerScreens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tcc.R;
+
+public class TelaUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_usuario);
+    }
+}
---
+++ app/src/main/java/com/example/tcc/ui/login/FormCadastro.java
@@ -0,0 +1,18 @@
+package com.example.tcc.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tcc.R;
+
+public class FormCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_cadastro);
+
+        //getSupportActionBar().hide();
+    }
+}
